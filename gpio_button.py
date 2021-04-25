0dbb52471850ccda6d999a2d8aba917fa6897a19
l00sed <danielwtompkins@gmail.com>
2021-04-25 18:47:54 -0400

    updated gpio_button script to detect each pedal

--- gpio_button.py
+++ gpio_button.py
@@ -1,10 +1,18 @@
 from gpiozero import Button
+from signal import pause
 
-button = Button(2)
+left_pedal = Button(2)
+center_pedal = Button(3)
+right_pedal = Button(26)
 
 while True:
-  if button.is_pressed:
-    print("Button is pressed")
-  else:
-    print("button is not pressed")
+  if left_pedal.is_pressed:
+    print("Left Pedal was Pressed")
+#left_pedal.when_released = print("Left Pedal was Released")
+  if center_pedal.is_pressed:
+    print("Center Pedal was Pressed")
+#center_pedal.when_released = print("Center Pedal was Not Pressed")
+  if right_pedal.is_pressed:
+    print("Right Pedal was Pressed")
+#right_pedal.when_released = print("Right Pedal was Released")
 
